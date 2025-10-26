c6d0779b4bdb5e21ba634e5603cc9a3ef756e820
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-10-26 23:16:53 +0530

    navbar done

--- src/app/components/UI/Navbar.tsx
+++ src/app/components/UI/Navbar.tsx
@@ -1,12 +1,18 @@
 "use client";
-import React, { useEffect, useState } from "react";
+
 import Image from "next/image";
+import { useState, useEffect } from "react";
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  // close mobile menu when resizing to large screens
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth >= 1024) {
+      if (typeof window !== "undefined" && window.innerWidth >= 1024) {
         setIsMenuOpen(false);
       }
     };
@@ -14,6 +20,7 @@ const Navbar = () => {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+
   const menuItems = [
     { name: "Home", href: "#home", hasDropdown: false },
     { name: "Plants Type", href: "#plants-type", hasDropdown: true },
@@ -23,7 +30,7 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="flex mt-6 w-full items-center justify-between mx-auto px-4 sm:px-8 lg:px-10 py-6 md:py-8 bg-transparent text-white">
+      <div className="flex mt-6 w-full items-center justify-between mx-auto px-4 sm:px-8 lg:px-10 py-2 md:py-4  text-white">
         {/* logo and name*/}
         <div className="flex shrink-0 items-center z-10 gap-2">
           <Image
@@ -41,25 +48,22 @@ const Navbar = () => {
         {/* menu list*/}
         <nav className="hidden lg:flex grow justify-center pt-3 ">
           <div className="flex grow justify-between items-center px-8">
-            <ul className="flex items-center gap-10 text-2xl font-normal indie mx-auto">
+            <ul className="flex items-center gap-10 text-[clamp(1.3rem,2vw,1.5rem)] font-normal indie mx-auto pr-20">
               {menuItems.map((item) => (
                 <li key={item.name} className="group relative">
-                  <a
-                    href={item.href}
-                    onClick={() => setIsMenuOpen(false)}
-                    className="hover:opacity-70"
-                  >
+                  <a href={item.href} className="hover:opacity-70">
                     {item.name}
+
+                    {item.hasDropdown && (
+                      <Image
+                        src="/arrowdown.png"
+                        alt=""
+                        width={15}
+                        height={7}
+                        className="inline-block ml-2 w-4 h-2"
+                      />
+                    )}
                   </a>
-                  {item.hasDropdown && (
-                    <Image
-                      src="/arrowdown.png"
-                      alt=""
-                      width={15}
-                      height={7}
-                      className="inline-block ml-2 w-4 h-2"
-                    />
-                  )}
                 </li>
               ))}
             </ul>
@@ -81,19 +85,84 @@ const Navbar = () => {
                   height={26}
                 ></Image>
               </li>
-              
             </ul>
           </div>
-          
         </nav>
-        <div className="  flex flex-col items-start pt-2 ">
-            <a href="#">
-              <div className="  h-0 border-t-4 border-white rounded-sm transition-all duration-300 ease-in-out w-[1.938rem] mb-2" />
 
-            <div className="h-0 border-t-4 border-white rounded-sm transition-opacity duration-300 ease-in-out w-[1.438rem]" />
-              </a>
+        {/* mobile menu options*/}
+        <div className=" flex items-end pt-2 z-50 ">
+          <button
+          type="button"
+            className="relative h-8 w-8 "
+            onClick={toggleMenu}
+            aria-label="Menu"
+            aria-expanded={isMenuOpen}
+          >
            
+           <div 
+              className={`
+                absolute top-1/2 right-0 h-0 border-t-4 border-white rounded-sm transition-all duration-300 w-[1.938rem]
+                ${isMenuOpen ? 'rotate-45' : '-translate-y-1.5'}`}
+            />
+            <div 
+              className={`
+                absolute top-1/2 right-0  h-0 border-t-4 border-white rounded-sm transition-all duration-300 w-[1.438rem]
+                ${isMenuOpen ? 'w-[1.938rem] -rotate-45' : 'translate-y-1.5'}
+                `}
+            />
+          </button>
+        </div>
+        <nav
+          className={`lg:hidden fixed top-0 left-0 w-full h-screen z-40 transition-transform duration-300 ease-in-out ${
+            isMenuOpen ? "translate-y-0" : "-translate-y-full"
+          }`}
+        >
+          <div className="bg-green-950 opacity-95 w-full h-full flex flex-col items-center justify-center">
+            <ul className="text-white text-center">
+              {menuItems.map((item) => (
+                <li
+                  key={item.name}
+                  className="group relative my-6 text-2xl indie"
+                >
+                  <a
+                    href={item.href}
+                    onClick={() => setIsMenuOpen(false)}
+                    className="hover:opacity-70"
+                  >
+                    {item.name}
+                  </a>
+                  {item.hasDropdown && (
+                    <Image
+                      src="/arrowdown.png"
+                      alt=""
+                      width={15}
+                      height={7}
+                      className="inline-block ml-2 w-4 h-2"
+                    />
+                  )}
+                </li>
+              ))}
+            </ul>
+            <ul className=" flex items-center gap-8 mt-6">
+              <li>
+                <button
+                  className="cursor-pointer"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  <Image src="/search-icon.png" alt="" width={26} height={26} />
+                </button>
+              </li>
+              <li>
+                <button
+                  className="cursor-pointer"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  <Image src="/bag-icon.png" alt="" width={26} height={26} />
+                </button>
+              </li>
+            </ul>
           </div>
+        </nav>
       </div>
     </>
   );

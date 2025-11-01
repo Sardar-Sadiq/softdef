9f0fd32a4df80af1434632d56b98e621490ab365
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-11-01 10:01:20 +0530

    working on smoothscroll

--- src/app/components/UI/SmoothScroll.tsx
+++ src/app/components/UI/SmoothScroll.tsx
@@ -4,7 +4,15 @@ import { ReactLenis } from '@studio-freight/react-lenis';
 
 function SmoothScroll({children}: { children: React.ReactNode}) {
     return (
-        <ReactLenis root options={{ lerp:0.15}}>
+        <ReactLenis root options={{ 
+            lerp:0.1,
+            smoothWheel: true,
+  
+    syncTouch: true,
+    duration: 1.2,  
+
+            
+        }}>
             {children}
         </ReactLenis>
     );
